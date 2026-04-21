3a19cd9a7c6c643271cb02856a69668cab114115
jaineet06 <jaineetshah06@gmail.com>
2026-04-21 21:07:07 +0530

    Bug resolved

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
-import { AppContextProvider } from '../../../React/my_portfolio/src/AppContext/AppContext.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { AppContextProvider } from "./context/AppContext.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <AppContextProvider>
-      <App/>
+      <App />
     </AppContextProvider>
   </BrowserRouter>
-
-)
+);
